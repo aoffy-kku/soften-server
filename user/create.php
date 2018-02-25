ea8060dd4aafeda4890d7841a8e034f9e90dd64c
Tanatnan Akka <33896588+pammiepp@users.noreply.github.com>
2018-02-25 16:43:20 +0700

    Create user create.php

---
+++ user/create.php
@@ -0,0 +1,30 @@
+<?php
+    require_once("../config/post_header.php");
+    include_once("../config/database.php");
+
+    include_once("../model/user.php");
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $user = new User($db);
+
+    // get posted data
+    // $data = json_decode(file_get_contents("php://input"));
+    // print_r($data);
+    $user->username = $_POST["username"];
+    $user->password = $_POST["password"];
+    $user->personal_id = $_POST["personal_id"];
+    
+    $result = $user->create();
+    if($result === true) {
+        echo json_encode(array(
+            "success" => true,
+        ));
+    } else {
+        echo json_encode(array(
+            "success" => false,
+            "message" => $result,
+        ));
+    }  
+?>
